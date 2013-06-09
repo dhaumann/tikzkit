7cbc88557bee0a93dbc2c578b322a8c6db36611f
Dominik Haumann <dhaumann@kde.org>
2013-06-09 22:45:54 +0200

    libraries: LGPLv2+, app: GPLv2+

--- src/app/DemoWidget.cpp
+++ src/app/DemoWidget.cpp
@@ -1,3 +1,20 @@
+/* This file is part of the TikZKit project
+ *
+ * Copyright (C) 2013 Dominik Haumann <dhaumann@kde.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 or the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; If not, see <http://www.gnu.org/licenses/>.
+ */
 #include "DemoWidget.h"
 
 #include "TikzNode.h"
