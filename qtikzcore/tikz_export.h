b3a9df76e69d3d539b3c342c5cc9d107664fb555
Dominik Haumann <dhaumann@kde.org>
2013-08-13 14:27:09 +0200

    simplify *_export.h makros

--- qtikzcore/tikz_export.h
+++ qtikzcore/tikz_export.h
@@ -20,27 +20,12 @@
 #ifndef TIKZ_EXPORT_H
 #define TIKZ_EXPORT_H
 
-// visibility
-#define HAVE_VISIBILITY
+#include <QtCore/QtGlobal>
 
-#ifdef HAVE_VISIBILITY
-# define PROPERTY_NO_EXPORT __attribute__ ((visibility("hidden")))
-# define PROPERTY_EXPORT __attribute__ ((visibility("default")))
-# define PROPERTY_IMPORT __attribute__ ((visibility("default")))
+#if defined(MAKE_TIKZCORE)
+#  define TIKZCORE_EXPORT Q_DECL_EXPORT
 #else
-# define PROPERTY_NO_EXPORT
-# define PROPERTY_EXPORT
-# define PROPERTY_IMPORT
-#endif
-
-#ifndef TIKZCORE_EXPORT
-# if defined(MAKE_TIKZCORE)
-/* We are building this library */
-#  define TIKZCORE_EXPORT PROPERTY_EXPORT
-# else
-/* We are using this library */
-#  define TIKZCORE_EXPORT PROPERTY_IMPORT
-# endif
+#  define TIKZCORE_EXPORT Q_DECL_IMPORT
 #endif
 
 #endif // TIKZ_EXPORT_H
--- qtikzgui/tikzgui_export.h
+++ qtikzgui/tikzgui_export.h
@@ -20,27 +20,12 @@
 #ifndef TIKZ_GUI_EXPORT_H
 #define TIKZ_GUI_EXPORT_H
 
-// visibility
-#define HAVE_VISIBILITY
+#include <QtCore/QtGlobal>
 
-#ifdef HAVE_VISIBILITY
-# define PROPERTY_NO_EXPORT __attribute__ ((visibility("hidden")))
-# define PROPERTY_EXPORT __attribute__ ((visibility("default")))
-# define PROPERTY_IMPORT __attribute__ ((visibility("default")))
+#if defined(MAKE_TIKZGUI)
+#  define TIKZGUI_EXPORT Q_DECL_EXPORT
 #else
-# define PROPERTY_NO_EXPORT
-# define PROPERTY_EXPORT
-# define PROPERTY_IMPORT
-#endif
-
-#ifndef TIKZGUI_EXPORT
-# if defined(MAKE_TIKZGUI)
-/* We are building this library */
-#  define TIKZGUI_EXPORT PROPERTY_EXPORT
-# else
-/* We are using this library */
-#  define TIKZGUI_EXPORT PROPERTY_IMPORT
-# endif
+#  define TIKZGUI_EXPORT Q_DECL_IMPORT
 #endif
 
 #endif // TIKZ_GUI_EXPORT_H
