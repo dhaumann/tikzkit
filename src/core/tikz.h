279858bac8261301469157e1cc8a46e3d707ebc6
Dominik Haumann <dhaumann@kde.org>
2014-04-05 19:57:40 +0200

    port TikZKit to Qt5

--- src/core/tikz.h
+++ src/core/tikz.h
@@ -20,12 +20,13 @@
 #define TIKZ_H
 
 // TIKZCORE_EXPORT: this like exists just to copy the include files
+#include <tikz_export.h>
 
 #include <QObject>
 #ifndef Q_MOC_RUN
 namespace tikz {
 #else
-class tikz {
+class TIKZCORE_EXPORT tikz {
     Q_GADGET
     Q_ENUMS(TextAlignment)
     Q_ENUMS(Anchor)
