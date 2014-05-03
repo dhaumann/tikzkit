9fe0df2035e30a405f39341f1d35644fae45a834
Dominik Haumann <dhaumann@kde.org>
2014-05-03 23:40:52 +0200

    add ColorWidget, showing a ColorPalette

---
+++ src/ui/colors/ColorWidget.cpp
@@ -0,0 +1,141 @@
+/* This file is part of the TikZKit project.
+ *
+ * Copyright (C) 2014 Dominik Haumann <dhaumann@kde.org>
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Library General Public License as published
+ * by the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#include "ColorWidget.h"
+#include "ColorPalette.h"
+
+#include <tikz/core/Style.h>
+#include <tikz/core/tikz.h>
+
+#include <QDebug>
+#include <QAbstractButton>
+#include <QButtonGroup>
+#include <QGridLayout>
+#include <QVBoxLayout>
+#include <QPainter>
+#include <QPaintEvent>
+#include <QVector>
+
+namespace tikz {
+namespace ui {
+
+class ColorButton : public QAbstractButton
+{
+public:
+    ColorButton(QRgb color, QWidget * parent = 0)
+        : QAbstractButton(parent)
+        , m_color(color)
+    {
+        setCheckable(true);
+    }
+
+    void paintEvent(QPaintEvent * ) override
+    {
+        QPainter p(this);
+        p.fillRect(QRect(1, 1, 16, 16), m_color);
+
+        if (isChecked() || underMouse()) {
+            p.drawRect(QRect(0, 0, 17, 17));
+        }
+    }
+
+    QRgb color() const noexcept
+    {
+        return m_color;
+    }
+
+    QSize sizeHint() const override
+    {
+        return QSize(18, 18);
+    }
+
+    QSize minimumSizeHint() const override
+    {
+        return sizeHint();
+    }
+
+private:
+    QRgb m_color;
+};
+
+class ColorWidgetPrivate
+{
+    public:
+        QVector<ColorButton*> buttons;
+        QButtonGroup * group;
+        QVBoxLayout * vLayout;
+        QGridLayout * gridLayout;
+};
+
+ColorWidget::ColorWidget(QWidget * parent)
+    : QFrame(parent)
+    , d(new ColorWidgetPrivate())
+{
+    d->group = new QButtonGroup(this);
+    d->vLayout = new QVBoxLayout(this);
+    d->gridLayout = new QGridLayout();
+    d->gridLayout->setSpacing(0);
+    d->vLayout->addLayout(d->gridLayout);
+
+    ColorPalette cp;
+//     cp.load("../data/palettes/tikzkit.gpl");
+    cp.load("../data/palettes/oxygen.gpl");
+//     cp.load("../data/palettes/oxygen-vibrant.gpl");
+    showPalette(cp);
+
+    connect(d->group, SIGNAL(buttonClicked(QAbstractButton*)),
+            this, SLOT(setColor(QAbstractButton*)));
+}
+
+ColorWidget::~ColorWidget()
+{
+    delete d;
+}
+
+void ColorWidget::showPalette(const ColorPalette & palette)
+{
+    if (d->buttons.size()) {
+        qDeleteAll(d->buttons);
+        d->buttons.clear();
+    }
+
+    d->buttons.reserve(palette.rows() * palette.columns());
+
+
+    for (int row = 0; row < palette.rows(); ++row) {
+        for (int col = 0; col < palette.columns(); ++col) {
+            auto button = new ColorButton(palette.color(row, col), this);
+            d->group->addButton(button);
+            d->gridLayout->addWidget(button, row, col);
+        }
+    }
+}
+
+void ColorWidget::setColor(QAbstractButton * button)
+{
+    auto colorButton = dynamic_cast<ColorButton *>(button);
+    Q_ASSERT(colorButton);
+
+    qDebug() << colorButton->color();
+}
+
+}
+}
+
+// kate: indent-width 4; replace-tabs on;
---
+++ src/ui/colors/ColorWidget.h
@@ -0,0 +1,75 @@
+/* This file is part of the TikZKit project.
+ *
+ * Copyright (C) 2014 Dominik Haumann <dhaumann@kde.org>
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Library General Public License as published
+ * by the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef TIKZUI_COLOR_WIDGET_H
+#define TIKZUI_COLOR_WIDGET_H
+
+#include "tikzgui_export.h"
+#include <QFrame>
+
+class QAbstractButton;
+
+namespace tikz {
+namespace ui {
+
+class ColorWidgetPrivate;
+class ColorPalette;
+
+/**
+ * Widget that shows a color palette.
+ *
+ * The ColorWidget represents a color palette.
+ * The color palette is loaded for a file by calling @p load().
+ * Thereafter, the colors can be obtained with @p color() with parameters
+ * in the range of rows() and columns().
+ */
+class TIKZUI_EXPORT ColorWidget : public QFrame
+{
+    Q_OBJECT
+
+    public:
+        /**
+         * Default constructor.
+         */
+        explicit ColorWidget(QWidget * parent = 0);
+
+        /**
+         * Destructor
+         */
+        ~ColorWidget();
+
+    public:
+        /**
+         * shows the color palette @p palette
+         */
+        void showPalette(const ColorPalette & palette);
+
+    private Q_SLOTS:
+        void setColor(QAbstractButton * button);
+
+    private:
+        ColorWidgetPrivate * const d;
+};
+
+}
+}
+
+#endif // TIKZUI_COLOR_WIDGET_H
+
+// kate: indent-width 4; replace-tabs on;
